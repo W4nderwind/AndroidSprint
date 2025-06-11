f1ac84da91301ea06c854c9fa8289467b1b925ab
cawah <cawa.hramenkov@mail.ru>
2025-06-11 20:26:42 +0300

    KS-3-1

---
+++ app/src/main/kotlin/practice/lesson_3_1 practice.kt
@@ -0,0 +1,16 @@
+package practice
+
+fun main (){
+
+    val userName = "Богдан"
+    var greeting = "Добрый день"
+
+    println("$greeting, $userName")
+
+    greeting = "Добрый вечер"
+
+    println("$greeting, $userName")
+
+
+
+}
