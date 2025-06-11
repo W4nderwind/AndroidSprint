bdf36a6785e23bec3cd0333e42e8401c5446bc31
cawah <cawa.hramenkov@mail.ru>
2025-06-11 20:27:41 +0300

    KS-3-4

---
+++ app/src/main/kotlin/practice/lesson_3_4 practice.kt
@@ -0,0 +1,21 @@
+package practice
+
+fun main() {
+
+    var start = "E2"
+    var end = "E4"
+    var moveNumber = 1
+
+    var stringToSend = "$start,$end,$moveNumber"
+
+    println(stringToSend)
+
+    start = "D2"
+    end = "D3"
+    moveNumber = 2
+
+    stringToSend = "$start,$end,$moveNumber"
+
+    println(stringToSend)
+
+}
