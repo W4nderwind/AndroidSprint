ef00911c9d27b01141f063ada8aa7d46483b16b9
cawah <cawa.hramenkov@mail.ru>
2025-06-11 17:31:31 +0300

    KS-1-5 refactor

--- app/src/main/kotlin/practice/lesson_1_5 practice.kt
+++ app/src/main/kotlin/practice/lesson_1_5 practice.kt
@@ -2,16 +2,18 @@ package practice
 
 fun main (){
     val seconds: Int = 6480
-    val minute: Int = seconds/60
-    val hour: Int = minute/60
-    val remainingMinutes = 48
-    val remainingSeconds: Int = 0
+    val minute: Int = seconds / timeFrame
+    val hour: Int = minute / timeFrame
 
-    print(0)
-    print(hour)
-    print(':')
-    print(remainingMinutes)
-    print(':')
-    print(0)
-    print(remainingSeconds)
+    val remainingMinutes = minute % timeFrame
+    val remainingSeconds: Int = seconds % timeFrame
+
+    val dateFormat = String.format("%02d:%02d:%02d", hour, remainingMinutes, remainingSeconds)
+
+    println(dateFormat)
+
+
+
+
 }
+const val timeFrame = 60
