b29cf5b4189193bc46508956bad54aca5b1b1890
W4nderwind <cawa.hramenkov@mail.ru>
2025-06-12 19:09:04 +0300

    KS-2-1 refactor

--- app/src/main/kotlin/practice/lesson 2_1 practice.kt
+++ app/src/main/kotlin/practice/lesson 2_1 practice.kt
@@ -2,14 +2,13 @@ package practice
 
 fun main (){
 
-    val numberOfStudents: Double = 4.0
+    val numberOfStudents = 4
     val assessmentIvan = 3
     val assessmentAnna = 4
     val assessmentBogdan = 3
     val assessmentKirill = 5
 
-    val arithmeticMean = (assessmentIvan + assessmentAnna + assessmentBogdan + assessmentKirill) / numberOfStudents
+    val arithmeticMean = (assessmentIvan + assessmentAnna + assessmentBogdan + assessmentKirill).toDouble() / numberOfStudents
 
     println(arithmeticMean)
-
 }
