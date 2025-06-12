c6215f359e5dc6bbaed3e7e11eddf140b636f73c
W4nderwind <cawa.hramenkov@mail.ru>
2025-06-12 19:57:04 +0300

    KS-2-4 refactor

--- app/src/main/kotlin/practice/lesson 2_4 practice.kt
+++ app/src/main/kotlin/practice/lesson 2_4 practice.kt
@@ -2,14 +2,19 @@ package practice
 
 fun main() {
 
-    val buff = 0.2
+    val buff = 20
     val crystal = 7
     val iron = 11
 
-    val crystalMiningBuff = (crystal * buff).toInt()
-    val ironMiningBuff = (iron * buff).toInt()
+    val buffPercent = buff / 100.0
+
+    val crystalMiningBuff = (crystal * buffPercent).toInt()
+    val ironMiningBuff = (iron * buffPercent).toInt()
+//    val crystalMiningBuff = (crystal * (buff / 100.0)).toInt()
+//    val ironMiningBuff = (iron * (buff / 100.0)).toInt()
 
     println(crystalMiningBuff)
     println(ironMiningBuff)
+
 
 }
