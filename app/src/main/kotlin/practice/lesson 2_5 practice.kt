f2f8a6bee302b1890907144c96dbb849f83f9df9
W4nderwind <cawa.hramenkov@mail.ru>
2025-06-12 20:06:11 +0300

    KS-2-5 refactor 2

--- app/src/main/kotlin/practice/lesson 2_5 practice.kt
+++ app/src/main/kotlin/practice/lesson 2_5 practice.kt
@@ -5,11 +5,11 @@ import kotlin.math.pow
 fun main (){
 
     val initialAmount = 70000
-    val interestRate = 0.167
+    val interestRate = 16.7
     val amountOfAccrualPerYear = 1
     val numberOfYears = 20
 
-    val totalAmount = initialAmount * (1 + interestRate / amountOfAccrualPerYear).pow(numberOfYears * amountOfAccrualPerYear)
+    val totalAmount = initialAmount * (1 + (interestRate / 100) / amountOfAccrualPerYear).pow(numberOfYears * amountOfAccrualPerYear)
 
     val result = "%.3f".format(totalAmount)
 
