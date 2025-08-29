cc6620936fba4e4dfaf06293f610db4dac61bf6f
W4nderwind <cawa.hramenkov@mail.ru>
2025-08-29 13:44:17 +0300

    KS-4-3 refactor

--- app/src/main/kotlin/practice/lesson_4_3 practice.kt
+++ app/src/main/kotlin/practice/lesson_4_3 practice.kt
@@ -7,10 +7,11 @@ fun main () {
     val airHumidity = 20
     val timeOfYear = "зима"
 
-    val dataVerification = sunnyWeather == true && openAwning == true && airHumidity == 20 && timeOfYear !== "зима"
+    val dataVerification = sunnyWeather == CORRECT_WEATHER && openAwning == CORRECT_AWNING && airHumidity == CORRECT_HUMIDITY && timeOfYear != UNFAVORABLE_TIME_OF_YEAR
 
     println("Благоприятные ли условия сейчас для роста бобовых? $dataVerification")
-
-
-
 }
+const val CORRECT_WEATHER = true
+const val CORRECT_AWNING = true
+const val CORRECT_HUMIDITY = 20
+const val UNFAVORABLE_TIME_OF_YEAR = "зима"
