5a008eb18a2d211bdcf461f76842596f4036f284
W4nderwind <cawa.hramenkov@mail.ru>
2025-09-03 14:00:52 +0300

    KS-2-3 refactor

--- app/src/main/kotlin/practice/lesson 2_3 practice.kt
+++ app/src/main/kotlin/practice/lesson 2_3 practice.kt
@@ -6,11 +6,11 @@ fun main() {
     val minute = 39
     val travelTime = 457
 
-    val totalMinutes = hour * TIME_FRAME + minute + travelTime
-    val arrivalHour = totalMinutes / TIME_FRAME
-    val arrivalMinute = totalMinutes % TIME_FRAME
+    val totalMinutes = hour * TIME_FRAME1 + minute + travelTime
+    val arrivalHour = totalMinutes / TIME_FRAME1
+    val arrivalMinute = totalMinutes % TIME_FRAME1
 
     println("$arrivalHour:$arrivalMinute")
 
 }
-const val TIME_FRAME = 60
+const val TIME_FRAME1 = 60
