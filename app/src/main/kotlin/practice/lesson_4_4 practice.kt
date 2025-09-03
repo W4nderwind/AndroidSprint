5ba952f2854c6d0b6258367eee28409b41a9b963
W4nderwind <cawa.hramenkov@mail.ru>
2025-09-03 12:59:31 +0300

    lesson_4_4 refactor

--- app/src/main/kotlin/practice/lesson_4_4 practice.kt
+++ app/src/main/kotlin/practice/lesson_4_4 practice.kt
@@ -5,12 +5,11 @@ fun main(){
     val trainingDay = 5
 
     val handsDay = trainingDay % 2 == 1
-    val legsDay = !handsDay
 
     println("""
         Упражнения для рук: $handsDay
-        Упражнения для ног: $legsDay
-        Упражнения для спины: $legsDay
+        Упражнения для ног: ${!handsDay}
+        Упражнения для спины: ${!handsDay}
         Упражнения для пресса: $handsDay
     """.trimIndent())
 
